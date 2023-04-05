c0ac3c24c8ed1ab0b90337855a7d565064f3ca84
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-05 16:42:25 +0200

    add diceRoll logic

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const heroClass = {
   name: "Wizard",
   avatar: "/images/wizard.png",
   health: 60,
-  diceRoll: [3, 1, 4],
   diceCount: 3,
 };
 const monsterClass = {
@@ -12,22 +11,32 @@ const monsterClass = {
   name: "Orc",
   avatar: "images/orc.png",
   health: 10,
-  diceRoll: [2],
   diceCount: 1,
 };
 
-// Render Page
-function renderCharacter(data) {
-  const { id, name, avatar, health, diceRoll, diceCount } = data;
-  let diceHtml = diceRoll
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 6) + 1);
+}
+
+function getDiceHtml(rolls) {
+  return getDiceRollArray(rolls)
     .map((roll) => `<div class="dice">${roll}</div>`)
     .join("");
+}
+
+// Render Page
+
+function renderCharacter(data) {
+  const { id, name, avatar, health, diceCount } = data;
+  let diceHtml = getDiceHtml(diceCount);
   document.getElementById(id).innerHTML = `<div class="character-card">
-         <h4 class="name">${name}</h4>
-         <img class="avatar" src="${avatar}"/>
-         <p class="health">health: <b>${health}</b></p>
-         <div class="dice-container">${diceHtml}</div>
-      </div>  `;
+          <h4 class="name">${name}</h4>
+          <img class="avatar" src="${avatar}"/>
+          <p class="health">health: <b>${health}</b></p>
+          <div class="dice-container">${diceHtml}</div>
+       </div>  `;
 }
 renderCharacter(heroClass);
 renderCharacter(monsterClass);
